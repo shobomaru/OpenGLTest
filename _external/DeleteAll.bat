6b0070c157b66ca8be0ea83209afa6525f14cef4
shobomaru <shobomaru@gmail.com>
2014-10-14 02:42:00 +0900

    Add new project and external libraries.

---
+++ _external/DeleteAll.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Delete all unzipped external libraries.
+
+rd /Q /S GLEW
+rd /Q /S GLFW
---
+++ _external/UnzipAll.bat
@@ -0,0 +1,5 @@
+REM Unzip all external libraries.
+
+@echo off
+"7-zip/7za" x GLEW.zip
+"7-zip/7za" x GLFW.zip
